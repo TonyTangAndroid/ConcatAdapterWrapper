0ca8eb3b3e6bd0f0b2b9444540f60d54da548896
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-13 14:33:08 -0800

    Add DataSourceTest.kt

---
+++ app/src/test/java/com/mindorks/example/mergeadapter/data/DataSourceTest.kt
@@ -0,0 +1,10 @@
+package com.mindorks.example.mergeadapter.data.com.mindorks.example.mergeadapter.data
+
+import org.junit.Assert.*
+
+class DataSourceTest {
+
+    @org.junit.Test
+    fun getUser() {
+    }
+}
